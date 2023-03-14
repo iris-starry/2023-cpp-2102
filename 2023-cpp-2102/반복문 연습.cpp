71b6044a662e5d3c4164a33158feb791cd235821
iris_ <iris_@SUNHEE_COM>
2023-03-14 09:44:40 +0900

    구구단 무식하게

---
+++ 2023-cpp-2102/반복문 연습.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("2 x 1 = 2\n");
+	printf("2 x 2 = 4\n");
+	printf("2 x 3 = 6\n");
+	printf("2 x 4 = 8\n");
+	printf("2 x 5 = 10\n");
+	printf("2 x 6 = 12\n");
+	printf("2 x 7 = 14\n");
+	printf("2 x 8 = 16\n");
+	printf("2 x 9 = 18\n");
+
+	printf("3 x 1 = 3\n");
+	printf("3 x 2 = 6\n");
+	printf("3 x 3 = 9\n");
+	printf("3 x 4 = 12\n");
+	printf("3 x 5 = 15\n");
+	printf("3 x 6 = 18\n");
+	printf("3 x 7 = 21\n");
+	printf("3 x 8 = 24\n");
+	printf("3 x 9 = 27\n");
+
+	printf("4 x 1 = 4\n");
+	printf("4 x 2 = 8\n");
+	printf("4 x 3 = 12\n");
+	printf("4 x 4 = 16\n");
+	printf("4 x 5 = 20\n");
+	printf("4 x 6 = 24\n");
+	printf("4 x 7 = 28\n");
+	printf("4 x 8 = 32\n");
+	printf("4 x 9 = 36\n");
+
+	return 0;
+}
