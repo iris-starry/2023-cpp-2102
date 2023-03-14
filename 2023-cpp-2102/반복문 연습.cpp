0a6233b7e77a260a2ac2d7083739ec64069ae956
iris_ <iris_@SUNHEE_COM>
2023-03-14 10:00:17 +0900

    완벽한 구구단

--- 2023-cpp-2102/반복문 연습.cpp
+++ 2023-cpp-2102/반복문 연습.cpp
@@ -3,15 +3,9 @@
 int main() {
 
 	for (int i = 2; i < 10; i++) {
-		printf("%d x 1 = %d\n", i, i * 1);
-		printf("%d x 2 = %d\n", i, i * 2);
-		printf("%d x 3 = %d\n", i, i * 3);
-		printf("%d x 4 = %d\n", i, i * 4);
-		printf("%d x 5 = %d\n", i, i * 5);
-		printf("%d x 6 = %d\n", i, i * 6);
-		printf("%d x 7 = %d\n", i, i * 7);
-		printf("%d x 8 = %d\n", i, i * 8);
-		printf("%d x 9 = %d\n", i, i * 9);
+		for (int j = 1; j < 10; j++) {
+		printf("%d x %d = %d\n", i, j, i * j);
+		}
 	}
 
 	return 0;
