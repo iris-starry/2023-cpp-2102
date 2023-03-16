6579e49dc2781916847936a52384c9798395c220
iris_ <iris_@SUNHEE_COM>
2023-03-16 10:46:05 +0900

    변수 출력 추가

--- 2023-cpp-2102/문법/CPP 기본.cpp
+++ 2023-cpp-2102/문법/CPP 기본.cpp
@@ -4,9 +4,8 @@ using namespace std;     //이름공간
 
 int main() {
 	// std::cout : 출력을 위한 객체
-	// << : 출력 연산자
+	// << : 출력 연산자5
 	// std::endl : 개행(줄바꿈) 객체
 	std::cout << "Hello world" << std::endl;
-
 	return 0;
 }
