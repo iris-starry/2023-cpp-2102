ecfbf3a2f95dce20a58e8d98cf1d8442ee34ec64
iris_ <iris_@SUNHEE_COM>
2023-03-16 10:15:14 +0900

    namespace

--- 2023-cpp-2102/문법/CPP 기본.cpp
+++ 2023-cpp-2102/문법/CPP 기본.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
+using namespace std;     //이름공간
+
 int main() {
 	// std::cout : 출력을 위한 객체
 	// << : 출력 연산자
+	// std::endl : 개행(줄바꿈) 객체
 	std::cout << "Hello world" << std::endl;
 
 	return 0;
