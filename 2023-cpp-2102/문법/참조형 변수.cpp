4a8f89342277c29bde8022668e404607e452f3d2
iris_ <iris_@SUNHEE_COM>
2023-03-21 09:05:18 +0900

    참고(reference)

--- 2023-cpp-2102/문법/참조형 변수.cpp
+++ 2023-cpp-2102/문법/참조형 변수.cpp
@@ -6,8 +6,8 @@ int main(void) {
 
 	int a = 30;     //a : 30
 
-	int* pa = &a;   //pa : a의 주소값
-	*pa = 100;      //pa가 가리키는 대상(a) : 100
+	int* ra = &a;   //int* pa = &a;
+	*ra = 100;      //*pa = 100
 
 	cout << "a의 값은 " << a << endl;
 
