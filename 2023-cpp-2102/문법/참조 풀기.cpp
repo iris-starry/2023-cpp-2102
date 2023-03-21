ca45327d201ded0ae47d511be6722cda2d0df832
iris_ <iris_@SUNHEE_COM>
2023-03-21 09:35:04 +0900

    참조로 바꾸기

--- 2023-cpp-2102/문법/참조 풀기.cpp
+++ 2023-cpp-2102/문법/참조 풀기.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-void 더하라2를(int* num) {
-	*num += 2;
+void 더하라2를(int& num) {
+	num += 2;
 }
 
 int main(void) {
 	int a = 3;
-	더하라2를(&a);
+	더하라2를(a);
 
 	cout << a << endl;
 	return 0;
