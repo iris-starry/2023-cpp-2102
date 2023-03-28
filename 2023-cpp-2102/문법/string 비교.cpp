b06324510c6212ea1352e44a982e28f9327c9ebe
iris_ <iris_starry_@naver.com>
2023-03-28 09:11:23 +0900

    C스타일 strcmp 문자열비교 헷갈려

--- 2023-cpp-2102/문법/string 비교.cpp
+++ 2023-cpp-2102/문법/string 비교.cpp
@@ -8,35 +8,17 @@ int main(void) {
 	char str1[50] = "Hello";
 	char str2[50] = "World";
 
-	const int s1_1 = strlen(str1);
-	const int s2_1 = strlen(str2);
-
-	// s1_1과 s2_1중에서 작은 값을 선택
-	const int l = s1_1 < s2_1 ? s1_1 : s2_1;
-
-	int i;
-	for (int i = 0; i < 1; i++) {
-		if (str1[i] < str2[i]) {
-			printf("str2 > str1 \n");
-			break;
-		}
-		else if (str1[1] > str2[i]) {
-			printf("str1 > str2 \n");
-			break;
-		}
+	int result = strcmp(str1, str2);
+	if (result == 1) {
+		printf("str2 > str1");
+	}
+	else if(result == -1){
+		printf("str2 < str1");
+	}
+	else if (result == 0) {
+		printf("str2 == str1");
 	}
 
-	if (i == 1) {
-		if (s2_1 < s1_1) {
-			printf("str2 > str1 \n");
-		}
-		else if (s1_1 > s2_1) {
-			printf("str2 < str1 \n");
-		}
-		else {
-			printf("str2 == str1 \n");
-		}
-	}
 
 	return 0;
 }
