2aa6fc776c17b542f28d2a63f1478296c8ae2b2d
iris_ <iris_starry_@naver.com>
2023-03-30 10:52:29 +0900

    class 접근제어자 public

--- 2023-cpp-2102/문법/클래스 기본.cpp
+++ 2023-cpp-2102/문법/클래스 기본.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 //시험X
 //여러 data들을 하나의 의미있는 구조체로 묶어서 관리
-//구조체, 클래스는 단어의 첫 글자를 대문자로 한다.
-struct Studnet {
+//구조체, 클래스는 일반적으로 단어의 첫 글자를 대문자로 합시다.
+//class는 디폴트가 private (struct는 디폴트가 public)
+class Studnet {
+public:
 	string name;
 
 	//성능때문에 hakbun을 문자열로 하지 않음
@@ -14,7 +16,9 @@ struct Studnet {
 	//정수는 비교연산을 한번에 할 수 있으나, 문자열은 글자수 만큼 반복하여 비교해야함
 	int hakbun;
 	int age;
-	int sex;                 //가독성과 유지보수를 위해 열거형(enum)으로 하는 것을 추천
+	//가독성과 유지보수를 위해 열거형(enum)으로 하는 것을 추천
+	//0: 남자, 1: 여자
+	int sex;               
 	string department;
 };
 
