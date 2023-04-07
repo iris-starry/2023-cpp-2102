db3a54d7e60ef7e2838cca379edefe1182045fba
iris_ <iris_starry_@naver.com>
2023-04-08 01:19:47 +0900

    연산자

--- 2023-cpp-2102/문법/클래스 기본.cpp
+++ 2023-cpp-2102/문법/클래스 기본.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+struct Actor {
+    string name;
+    string dialog;
+};
+
 //여러 data들을 하나의 의미있는 구조체로 묶어서 관리
 //구조체,클래스는 일반적으로 단어의 첫 글자를 대문자로 한다
 //class는 default가 private(struct는 default가 public)--시험
@@ -12,20 +17,20 @@ public:
     //생성자(constructor) : 객체가 생성될 때 호출되는 함수
     //생성자를 정의하지 않으면 자동(default)으로 매개변수가 없는 생성자가 정의된다
     //Student(){ }
-    Student() {
-        name = "김선희";
-        student_num = 2102;
-        age = 18;
-        gender = 1;
-        department = "소프트웨어과";
+    //Student() {
+        // name = "김선희";
+        //student_num = 2102;
+        //age = 18;
+        // gender = 1;
+        //department = "소프트웨어과";
 
-    }
+    //}
     Student(string name, int student_num, int age, int gender, string department) {
-        this->name = name;
-        this->student_num = student_num;
-        this->age = age;
-        this->gender = gender;
-        this->department = department;
+        name = name;
+        student_num = student_num;
+        age = age;
+        gender = gender;
+        department = department;
     }
     //class는 멤버변수를 가질 수 있다--멤버함수
     void print(void)
@@ -52,10 +57,18 @@ private:
 int main() {
 
     //(struct drink)자료형(C++에서는 drink만 써도  됨)/struct 자체가 자료형
-    Student stu1 = Student();
-    stu1.print();
-    Student stu2 = Student("호빵맨", 2222, 18, 1, "뉴미디어 소프트웨어과");
-    stu2.print();
+    Student stu1 = Student("김선희", 2102, 18, 1, "뉴미디어 소프트웨어과");
+
+    struct Actor KYLee;
+    KYLee.name = "이경영";
+    KYLee.dialog = "진행시켜";
+
+    struct Actor* ptr = &KYLee;
+
+    //ptr->name, (*ptr)name 같은 결과
+    // ->: 축약
+    cout << "이름: " << ptr->name << endl;
+    cout << "대사: " << (*ptr).dialog << endl;
 
     return 0;
 }
