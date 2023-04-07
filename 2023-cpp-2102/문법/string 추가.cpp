ac390d56e43709f1b7b6f31a818a4048414e821e
iris_ <iris_starry_@naver.com>
2023-04-08 00:05:50 +0900

    객체를 선언할 때, class를 빼도 된다

--- 2023-cpp-2102/문법/string 추가.cpp
+++ 2023-cpp-2102/문법/string 추가.cpp
@@ -1,56 +1,8 @@
-#include<iostream>
-#include<string>
-
-using namespace std;
-
-
-struct Actor {
-	string name;
-	string dialog;
-};
-
-//시험X
-//여러 data들을 하나의 의미있는 구조체로 묶어서 관리
-//구조체, 클래스는 일반적으로 단어의 첫 글자를 대문자로 합시다.
-//class는 디폴트가 private (struct는 디폴트가 public)
-
-class Studnet {
-public:
-	string name;
-
-	//성능때문에 hakbun을 문자열로 하지 않음
-	//일반적으로 문자열은 정수데이터보다 많은 메모리 공간을 요구하며
-	//정수는 비교연산을 한번에 할 수 있으나, 문자열은 글자수 만큼 반복하여 비교해야함
-	int hakbun;
-	int age;
-	//가독성과 유지보수를 위해 열거형(enum)으로 하는 것을 추천
-	//0: 남자, 1: 여자
-	int sex;               
-	string department;
-
-	//class는 멤버변수를 가질 수 있다.
-	void print(void) {
-		cout << "이름 " << name << endl;
-		cout << "학번 " << name << endl;
-		cout << "나이 " << name << endl;
-		cout << "성별(0)남자, (1)여자 " << name << endl;
-		cout << "학과 " << name << endl;
-	}
-
-};
-
-
+#include<stdio.h>     //문자열 관련 함수
+#include<string.h>   //std::string이 들어있음(.h안돼)
 int main(void) {
+	char str[30] = "jiwoo";
+	char str2[30] = "rock";
 
-	// (struct drink)자료형(c++에서는 drink만 써도 됩니다.)
-	struct Studnet stu1;
-	stu1.name = "박남길";
-	stu1.hakbun = 200;
-	stu1.age = 18;
-	stu1.sex = 0;
-	stu1.department = "뉴미디어소프트웨어과";
-
-	stu1.print();
-	
 	return 0;
 }
--- 2023-cpp-2102/문법/클래스 기본.cpp
+++ 2023-cpp-2102/문법/클래스 기본.cpp
@@ -3,54 +3,40 @@
 
 using namespace std;
 
-
-struct Actor {
-	string name;
-	string dialog;
-};
-
-//시험X
-//여러 data들을 하나의 의미있는 구조체로 묶어서 관리
-//구조체, 클래스는 일반적으로 단어의 첫 글자를 대문자로 합시다.
-//class는 디폴트가 private (struct는 디폴트가 public)
-
-class Studnet {
+// 서로 관련된 여러 data들을 하나의 의미있는 구조체로 묶어서 관리
+// 구조체, 클래스는 일반적으로 단어의 첫 글자를 대문자로 합시다
+// class는 디폴트가 private (sttuct는 디폴트가 public)
+class Student {
 public:
-	string name;
+    string name;
+    int id;
+    // 성능 때문에 string으로 하지 않는다.
+    // 일반적으로 string은 정수데이터보다 많은 메모리 공간을 요구하며
+    // 정수는 비교연산을 한번에 할 수 있으나, 문자열은 글자 수 만큼 반복하여 비교해야함
+    int age;
+    // 가독성과 유지보수를 위해 열거형(enum)으로 하는 것을 추천
+    // 남자 : 0 , 여자 : 1
+    int sex;
+    string department;
 
-	//성능때문에 hakbun을 문자열로 하지 않음
-	//일반적으로 문자열은 정수데이터보다 많은 메모리 공간을 요구하며
-	//정수는 비교연산을 한번에 할 수 있으나, 문자열은 글자수 만큼 반복하여 비교해야함
-	int hakbun;
-	int age;
-	//가독성과 유지보수를 위해 열거형(enum)으로 하는 것을 추천
-	//0: 남자, 1: 여자
-	int sex;               
-	string department;
-
-	//class는 멤버변수를 가질 수 있다.
-	void print(void) {
-		cout << "이름 " << name << endl;
-		cout << "학번 " << name << endl;
-		cout << "나이 " << name << endl;
-		cout << "성별(0)남자, (1)여자 " << name << endl;
-		cout << "학과 " << name << endl;
-	}
-
+    void print(void) // 매개변수가 필요하지 않으니 (void)
+    {
+        cout << "이름 : " << name << endl;
+        cout << "학번 : " << id << endl;
+        cout << "나이 : " << age << endl;
+        cout << "성별 (0)남자, (1)은 여자 : " << sex << endl;
+        cout << "학과 : " << department << endl;
+    }
 };
 
-
-int main(void) {
-
-	// (struct drink)자료형(c++에서는 drink만 써도 됩니다.)
-	struct Studnet stu1;
-	stu1.name = "박남길";
-	stu1.hakbun = 200;
-	stu1.age = 18;
-	stu1.sex = 0;
-	stu1.department = "뉴미디어소프트웨어과";
-
-	stu1.print();
-	
-	return 0;
+int main() {
+    class Student s1;
+    Student s1;
+    s1.name = "김선희";
+    s1.age = 18;
+    s1.id = 2102;
+    s1.sex = 1;
+    s1.department = "뉴미디어 소프트웨어과";
+    s1.print();
+    return 0;
 }
