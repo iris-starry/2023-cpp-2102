bc0f5c8d8d5176f7145bebf9fdb39ee7ba20ad56
iris_ <iris_starry_@naver.com>
2023-04-18 10:29:23 +0900

    문자열 동적할당

--- 2023-cpp-2102/문법/클래스 중급.cpp
+++ 2023-cpp-2102/문법/클래스 중급.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
+#include <string>
+#include <string.h>
 
 using namespace std;
 
 class MString {
 public:
-	MString() {}
-	MString(const char* str) {
-		c_str_ = str;
+	MString(const char* str)
+	{
+		unsigned int str_length = strlen(str);
+		// 필요한 길이만큼 문자열 동적할당
+		c_str_ = new char[str_length + 1];	// '/0'(널문자) 공간 +1
+		strcpy(c_str_, str);
 	}
+
 	unsigned int size() { return size_; }
-	const char* c_str() { return c_str_; }
+	char* c_str() { return c_str_; }
 private:
-	unsigned int size_;  // 문자열의 길이
-	const char* c_str_;  // 문자열을 가리키는 주소
+	unsigned int size_;			// 문자열의 길이
+	char* c_str_;				// 문자열을 가리키는 주소
 };
 
-int main(void) {
+int main(void)
+{
 	MString str = MString("yakitori tabetai");
 
 	cout << str.c_str() << endl;
 
 	return 0;
+
 }
