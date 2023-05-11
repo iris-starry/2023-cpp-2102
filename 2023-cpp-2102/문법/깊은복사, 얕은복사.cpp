165b23451eda75e772ca87fd4dc5943819da60fb
iris_ <iris_starry_@naver.com>
2023-05-11 15:20:37 +0900

    깊은 복사

--- 2023-cpp-2102/문법/깊은복사, 얕은복사.cpp
+++ 2023-cpp-2102/문법/깊은복사, 얕은복사.cpp
@@ -7,10 +7,22 @@ int main(void) {
 	int* ptr1 = new int[3];
 	ptr1[0] = 0, ptr1[1] = 10, ptr1[2] = 20;
 
-	// ptr2는 별도의 동적할당을 하지 않고 ptr1의 주소값만 복사한다.
-	int* ptr2 = ptr1;
+	// ptr2는 별도의 동적할당을 한 후 ptr1이 가리키는 값들을 복사한다.
+	// 이러한 복사를 깊은복사(deep copy)
+	int* ptr2 = new int[3];
+	for (int i = 0; i < 3; i++) {
+		ptr2[i] = ptr1[i];
+	}
+
+	// 깊은 복사를 하면 고유의 공간을 가지므로
+	// ptr2[i]은 100으로 바뀌지 않는다.
+	ptr1[1] = 100;
 	for (int i = 0; i <= 2; i++) {
 		cout << ptr1[i] << " " << ptr2[i] << endl;
 	}
+
+	delete[] ptr1;
+	delete[] ptr2;
+
 	return 0;	
 }
