e5760e335cef36bc6d71f76aee8f75aefb0168ec
iris_ <iris_starry_@naver.com>
2023-05-16 09:39:14 +0900

    소멸자도 만들어보자

--- 2023-cpp-2102/문법/복사생성사.cpp
+++ 2023-cpp-2102/문법/복사생성사.cpp
@@ -14,6 +14,17 @@ public:
 		cout << "일반생성자 호출" << endl;
 	}
 
+	// 복사생성사
+	Munja(const Munja& m) : str_(m.str_)
+	{
+		cout << "복사생성자 호출" << endl;
+	}
+
+	// 소멸자
+	~Munja() {
+		delete[] str_;
+		cout << "소멸자 호출" << endl;
+	}
 private:
 	char* str_;
 };
@@ -22,6 +33,7 @@ private:
 
 int main(void) {
 
-	Munja m1 = Munja("abc");
+	Munja m1 = Munja("abc");  // 일반 생성자 호출
+	Munja m2 = m1;			  // 복사 생성자 호출
 
 }
