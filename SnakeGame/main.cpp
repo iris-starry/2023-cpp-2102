9c9ac9a77bebe92cc8226785c55120c62acd0804
iris_ <iris_starry_@naver.com>
2023-05-18 10:43:44 +0900

    [SFML] 윈도우 X를 누르면 창이 닫힘

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -5,5 +5,16 @@ using namespace sf;
 int main(void) {
 	sf::RenderWindow  window(VideoMode(640, 480), "Snake Game");
 
+	while (window.isOpen()) {
+		Event e;
+		while (window.pollEvent(e)) {
+			// 윈도우의 x를 눌렀을 때 창이 닫아지도록
+			if (e.type == Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.display();
+	}
+
 	return 0;
 }
