6f873138c785a7c99768da8255798fc9fa7f55cc
iris_ <iris_starry_@naver.com>
2023-05-23 10:18:13 +0900

    [SFML] 좌표 체계를 그리드화

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -9,24 +9,31 @@ int main(void) {
 	const int WIDTH = 1000;
 	const int HEIGHT = 800;
 
-	// 한 칸을 40으로
-	int block = 40;
+	int block = 40; // 한 칸을 40으로
+	const int w = WIDTH / block;
+	const int h = HEIGHT / block;
+
+
 
 	RenderWindow  window(VideoMode(WIDTH, HEIGHT), "Snake Game");
 	// 1초에  60번의 작업이 이루어 지도록 frame 조절
 	// 컴퓨터 사양이 달라도 똑같은 속도로 처리함
-	window.setFramerateLimit(60);
+	window.setFramerateLimit(30);
 
 	srand(time(NULL));
 
 	RectangleShape snake;
-	snake.setPosition(200, 300);
-	snake.setSize(Vector2f(30, 30));
+	int snake_x = 3;
+	int snake_y = 3;
+	snake.setPosition(snake_x * block , snake_y * block);
+	snake.setSize(Vector2f(block, block));
 	snake.setFillColor(Color::Green);
 
 	RectangleShape apple;
-	apple.setPosition(rand()%WIDTH-block, rand()%HEIGHT-block);
-	apple.setSize(Vector2f(30, 30));
+	int apple_x = rand() % w;
+	int apple_y = rand() % h;
+	apple.setPosition(apple_x * block, apple_y * block);
+	apple.setSize(Vector2f(block, block));
 	apple.setFillColor(Color::Red);
 	
 	while (window.isOpen()) {
@@ -39,20 +46,30 @@ int main(void) {
 
 		// input
 		// 네 개의 방향키가 중복으로 input되면 안됨
-		if (Keyboard::isKeyPressed(Keyboard::Up))
-			snake.move(0, -5);
-		else if (Keyboard::isKeyPressed(Keyboard::Down))
-			snake.move(0, 5);
-		else if (Keyboard::isKeyPressed(Keyboard::Right))
-			snake.move(5, 0);
-		else if (Keyboard::isKeyPressed(Keyboard::Left))
-			snake.move(-5, 0);
+		if (Keyboard::isKeyPressed(Keyboard::Up)) {
+			snake_y--;
+			snake.move(0, -block);
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Down)) {
+			snake_y++;
+			snake.move(0, block);
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Right)) {
+			snake_x++;
+			snake.move(block, 0);
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Left)) {
+			snake_x--;
+			snake.move(-block, 0);
+		}
 
 		// update
 
 		// 뱀이 사과를 먹으면
 		if (snake.getGlobalBounds().intersects(apple.getGlobalBounds())) {
-			apple.setPosition(rand() % WIDTH - block, rand() % HEIGHT - block);
+			apple_x = rand() % w;
+			apple_y = rand() % h;
+			apple.setPosition(apple_x * block, apple_y * block);
 		}
 
 		// render
