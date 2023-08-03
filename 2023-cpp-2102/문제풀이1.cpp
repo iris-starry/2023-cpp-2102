927ba893c932ad301030321ebb8e4b7963c22c50
sunhee kim <iris_starry_@naver.com>
2023-08-03 10:13:38 +0900

    문제풀이 1-2 깊은복사

---
+++ 2023-cpp-2102/문제풀이1.cpp
@@ -0,0 +1,31 @@
+class IntArray {
+public:
+	IntArray(int* arr, int size)
+	{
+		arr_ = new int[size];
+		size_ = size;
+		for (int i = 0; i < size; i++)
+			arr_[i] = arr[i];
+	}
+
+	// 깊은 복사 생성자
+	IntArray(const IntArray& rhs)
+	{
+		arr_ = new int[rhs.size_];
+		size_ = rhs.size_;
+		for (int i = 0; i < rhs.size_; i++)
+			arr_[i] = rhs.arr_[i];
+	}
+
+private:
+	int* arr_;
+	int size_; // arr의 크기
+};
+
+int main(void)
+{
+	int arr[] = { 1, 2, 3 };
+	IntArray ia = IntArray(arr, sizeof(arr) / sizeof(*arr));
+	IntArray ca = ia;		// 복사생성자
+	return 0;
+}
