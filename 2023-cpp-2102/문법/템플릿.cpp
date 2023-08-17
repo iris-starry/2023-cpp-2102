3497ec285e4fcc27654d5831a30a6b72f3fb6560
sunhee kim <iris_starry_@naver.com>
2023-08-17 11:14:36 +0900

    템플릿 적용 전

---
+++ 2023-cpp-2102/문법/템플릿.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int sum(int a, int b)
+{
+	return a + b;
+}
+
+float sum(float a, float b)
+{
+	return a + b;
+}
+
+string sum(string a, string b)
+{
+	return a + b;
+}
+
+int main(void)
+{
+	cout << sum(1, 2) << endl;
+	cout << sum(1.f, 2.f) << endl;
+	cout << sum("1", "2") << endl;
+}
