5924fc8e2d837c26bdb286b4201f852c9277b6a3
sunhee kim <iris_starry_@naver.com>
2023-08-17 11:16:16 +0900

    템플릿 적용 후

--- 2023-cpp-2102/문법/템플릿.cpp
+++ 2023-cpp-2102/문법/템플릿.cpp
@@ -3,24 +3,15 @@
 
 using namespace std;
 
-int sum(int a, int b)
-{
-	return a + b;
-}
-
-float sum(float a, float b)
-{
-	return a + b;
-}
-
-string sum(string a, string b)
+template <typename T>
+T sum(T a, T b)
 {
 	return a + b;
 }
 
 int main(void)
 {
-	cout << sum(1, 2) << endl;
-	cout << sum(1.f, 2.f) << endl;
-	cout << sum("1", "2") << endl;
+	cout << sum<int>(1, 2) << endl;
+	cout << sum<float>(1.f, 2.f) << endl;
+	cout << sum<string>("1", "2") << endl;
 }
