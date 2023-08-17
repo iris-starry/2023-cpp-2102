358614cdf79a6a0c5143e030309845c6e7627d71
sunhee kim <iris_starry_@naver.com>
2023-08-17 11:17:27 +0900

    클래스에 템플릿 적용

---
+++ 2023-cpp-2102/문법/템플릿 클래스.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+class MyData {
+public:
+	MyData(T data) : data_(data)
+	{
+	}
+
+	T get_data(void) { return data_; }
+	void set_data(T data) { data_ = data; }
+private:
+	T data_;
+};
+
+int main(void)
+{
+	MyData<int> md = MyData<int>(3);
+	cout << md.get_data() << endl;
+}
