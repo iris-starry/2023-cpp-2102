6d54ed3153cb4db6722ec30a875a9bab6dfe14f3
sunhee kim <iris_starry_@naver.com>
2023-09-12 08:44:20 +0900

    예전에 했던 상속 그래픽화

--- Inheritance/상속.cpp
+++ Inheritance/상속.cpp
@@ -1,70 +1,346 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-class Shape {
+#include <stdlib.h>
+
+#include <time.h>
+
+
+
+
+
+
+
+using namespace sf;
+
+
+
+
+class Entity {
+
 public:
-    virtual int get_point_count() const = 0; // 꼭지점의 개수를 반환하는 순수 가상 함수
-    virtual void get_position() const = 0;   // 위치를 출력하는 순수 가상 함수
+
+	// 거의 99.99%이상 클래스를 매개변수로 할 때는 주소값으로 받자
+
+	// 메모리 용량 및 call by value 이슈 때문에
+
+	Entity(int life, int speed, RectangleShape* sprite)
+
+		: life_(life), sprite_(sprite), speed_(speed)
+
+	{
+
+	}
+
+
+
+
+	~Entity() {}
+
+
+
+
+	// 스프라이트의 위치를 x, y만큼 변경
+
+	void move(float x, float y)
+
+	{
+
+		sprite_->move(x, y);
+
+	}
+
+
+
+
+	// 실제로 사용하는 것은 Player, Enemy에서 사용
+
+	virtual void eat(Entity* e) {}
+
+
+
+
+	// getter
+
+	int get_life() { return life_; }
+
+	int get_speed() { return speed_; }
+
+	RectangleShape get_sprite() { return *sprite_; }
+
+
+
+
+	// setter
+
+	void set_life(int val) { life_ = val; }
+
+	void set_speed(int val) { speed_ = val; }
+
+	void set_sprite(RectangleShape* val) { sprite_ = val; }
+
+
+
+
+	//private:
+
+	int life_;
+
+	int speed_;
+
+	RectangleShape* sprite_;
+
 };
 
-class RectangleShape : public Shape {
+
+
+
+class Player : public Entity {
+
+public:
+
+	Player(int life, int speed, RectangleShape* sprite, int score)
+
+		: Entity(life, speed, sprite), score_(score)
+
+	{
+
+	}
+
+
+
+
+	// 상대방의 life를 1 깎고, 점수를 획득
+
+	void eat(Entity* e) override
+
+	{
+
+		e->set_life(e->get_life() - 1);
+
+		score_ += 50;
+
+	}
+
 private:
-    double width;  // 너비
-    double height; // 높이
 
-public:
-    RectangleShape(double w, double h) : width(w), height(h) {}
+	int score_;
 
-    int get_point_count() const override {
-        return 4; // 사각형은 4개의 꼭지점을 가지고 있음
-    }
-
-    void get_position() const override {
-        std::cout << "사각형의 위치: (x, y)" << std::endl;
-    }
-
-    double get_width() const {
-        return width;
-    }
-
-    double get_height() const {
-        return height;
-    }
 };
 
-class CircleShape : public Shape {
-private:
-    double radius; // 반지름
 
-public:
-    CircleShape(double r) : radius(r) {}
 
-    int get_point_count() const override {
-        return 0; // 원형은 꼭지점이 없음
-    }
 
-    void get_position() const override {
-        std::cout << "원의 위치: (x, y)" << std::endl;
-    }
+int main(void)
 
-    double get_radius() const {
-        return radius;
-    }
-};
+{
 
-int main() {
-    // 객체 생성
-    RectangleShape rectangle(5.0, 3.0);
-    CircleShape circle(2.5);
+	srand((unsigned int)time(NULL));
 
-    // 사각형 정보 출력
-    std::cout << "사각형의 꼭지점 개수: " << rectangle.get_point_count() << std::endl;
-    rectangle.get_position();
-    std::cout << "사각형의 너비: " << rectangle.get_width() << ", 높이: " << rectangle.get_height() << std::endl;
 
-    // 원 정보 출력
-    std::cout << "원의 꼭지점 개수: " << circle.get_point_count() << std::endl;
-    circle.get_position();
-    std::cout << "원의 반지름: " << circle.get_radius() << std::endl;
 
-    return 0;
+
+	RenderWindow window(VideoMode(1000, 800), "Sangsok");
+
+	window.setFramerateLimit(60);
+
+
+
+
+	RectangleShape sp1;
+
+	sp1.setPosition(400, 300);
+
+	sp1.setSize(Vector2f(50, 50));
+
+	sp1.setFillColor(Color::Blue);
+
+
+
+
+	RectangleShape se1;
+
+	se1.setPosition(rand() % 800, rand() % 600);
+
+	se1.setSize(Vector2f(40, 40));
+
+	se1.setFillColor(Color::Yellow);
+
+
+
+
+	RectangleShape se2;
+
+	se2.setPosition(rand() % 800, rand() % 600);
+
+	se2.setSize(Vector2f(60, 60));
+
+	se2.setFillColor(Color::Color(128, 60, 100));
+
+
+
+
+	RectangleShape se3;
+
+	se3.setPosition(rand() % 800, rand() % 600);
+
+	se3.setSize(Vector2f(20, 20));
+
+	se3.setFillColor(Color::Color(42, 188, 100));
+
+
+
+
+
+
+
+	Player* player = new Player(3, 5, &sp1, 0);
+
+
+
+
+	Entity* enemy1 = new Entity(1, 3, &se1);
+
+	Entity* enemy2 = new Entity(1, 2, &se2);
+
+	Entity* enemy3 = new Entity(1, 4, &se3);
+
+
+
+
+	while (window.isOpen())
+
+	{
+
+		Event e;
+
+		while (window.pollEvent(e))
+
+		{
+
+			if (e.type == Event::Closed) {
+
+				window.close();
+
+			}
+
+		}
+
+		// Input
+
+		// 방향키 이동
+
+		if (Keyboard::isKeyPressed(Keyboard::Up)) {
+
+			player->move(0, -player->get_speed());
+
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Down)) {
+
+			player->move(0, player->get_speed());
+
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Right)) {
+
+			player->move(player->get_speed(), 0);
+
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Left)) {
+
+			player->move(-player->get_speed(), 0);
+
+		}
+
+
+
+
+
+
+
+		// Update
+
+
+
+
+		// player가 enemy1과 닿았을 때,
+
+		if (player->get_sprite().getGlobalBounds().intersects(enemy1->get_sprite().getGlobalBounds()))
+
+		{
+
+			player->eat(enemy1);
+
+		}
+
+		if (player->get_sprite().getGlobalBounds().intersects(enemy2->get_sprite().getGlobalBounds()))
+
+		{
+
+			player->eat(enemy2);
+
+		}
+
+		if (player->get_sprite().getGlobalBounds().intersects(enemy3->get_sprite().getGlobalBounds()))
+
+		{
+
+			player->eat(enemy3);
+
+		}
+
+
+
+
+		// Render
+
+		window.clear();
+
+
+
+
+		if (enemy1->get_life() > 0) {
+
+			window.draw(enemy1->get_sprite());
+
+		}
+
+		if (enemy2->get_life() > 0) {
+
+			window.draw(enemy2->get_sprite());
+
+		}
+
+		if (enemy3->get_life() > 0) {
+
+			window.draw(enemy3->get_sprite());
+
+		}
+
+
+
+
+		window.draw(player->get_sprite());
+
+
+
+
+		window.display();
+
+	}
+
+
+
+
+	delete player;
+
+	delete enemy1;
+
+	delete enemy2;
+
+	delete enemy3;
+
+	return 0;
+
 }
